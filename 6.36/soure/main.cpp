facc1f1bfcdfef866a423d2b8444e343b3b1286b
106360238 <john.01y281@gmaul.com>
2017-12-04 22:57:59 +0800

    compelte

---
+++ 6.36/soure/main.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char a[100];
+void stringReverse(int x);
+int main()
+{
+	printf("½Ð¿é¤J¤å¦r:");
+	scanf("%s", &a);
+	int x = strlen(a);
+	stringReverse(x);
+	system("pause");
+	return 0;
+}
+void stringReverse(int x)
+{
+	if (x == 1)
+	{
+		printf("%c", a[0]);
+
+	}
+	else
+	{
+		printf("%c", a[x - 1]);
+		stringReverse(x - 1);
+	}
+}
